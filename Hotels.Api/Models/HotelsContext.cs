276287f34db409e17614442ec494b707aedf6298
collectoranton <anton.lilja@collectorbank.se>
2018-05-09 10:20:34 +0200

    Basic structure

---
+++ Hotels.Api/Models/HotelsContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hotels.Api.Controllers
+{
+    public class HotelsContext : DbContext
+    {
+        public DbSet<Region> Regions { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                // Om webappen skapar en SamuraiContext så kommer inte detta köras
+                // Detta är default. Körs alltså när du använda Update-Database eller från EfSamurai.App-projektet
+
+                optionsBuilder.UseSqlServer("Server = (localdb)\\mssqllocaldb; Database = Hotels; Trusted_Connection = True;");
+            }
+
+            optionsBuilder.EnableSensitiveDataLogging();
+        }
+    }
+}
---
+++ Hotels.Api/Models/Region.cs
@@ -0,0 +1,9 @@
+﻿namespace Hotels.Api.Controllers
+{
+    public class Region
+    {
+        public int Id { get; set; }
+        public int RegionCode { get; set; }
+        public string Name { get; set; }
+    }
+}
