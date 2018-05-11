0b14de27e80b4e0c6afabccd28505569cd4de8bf
collectoranton <anton.lilja@collectorbank.se>
2018-05-11 16:43:12 +0200

    Last stuff on da friday

--- Hotels.Api/Startup.cs
+++ Hotels.Api/Startup.cs
@@ -6,6 +6,7 @@ using Hotels.Api.Controllers;
 using Hotels.Api.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -25,8 +26,11 @@ namespace Hotels.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddEntityFrameworkSqlServer().AddDbContext<HotelsContext>();
-            services.AddSingleton<RegionsRepository>();
+            //services.AddEntityFrameworkSqlServer().AddDbContext<HotelsContext>();
+            //services.AddSingleton<RegionsRepository>();
+
+            services.AddDbContext<HotelsContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc();
         }
 
